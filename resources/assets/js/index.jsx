d9bdfe6a088cc1639c6069a17c212c3daa3ab34f
josedanCanaima <josedanq100@gmail.com>
2018-06-14 12:42:36 -0400

    config styles enviroment

--- resources/assets/js/index.jsx
+++ resources/assets/js/index.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 
 import Nav from "./components/Nav";
 
-require("../sass/app");
-
 ReactDOM.render(
     <Nav />,
     document.getElementById("nav")
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,49 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 // const webpack = require('webpack');
 
-const config = {
+module.exports = (env) => {
+	return {
+		// entry: './resources/assets/js/index.jsx',
+		entry: {
+			// config: './resources/assets/js/config.js',
+			// styles: './resources/assets/sass/app.scss',
+			index: './resources/assets/js/index.jsx'
+		},
 
-	// entry: './resources/assets/js/index.jsx',
-	entry: {
-		// config: './resources/assets/js/config.js',
-		index: ['./resources/assets/js/index.jsx', './resources/assets/sass/app.scss']	
-	},
+		output: {
+			path: path.resolve(__dirname, 'public/js/'),
+			publicPath: '/public/js/',
+			filename: '[name].min.js' // Archivo de salida
+		},
 
-	output: {
-	    path: path.resolve(__dirname, 'public/js/'),
-		publicPath: '/public/js/',
-	    filename: '[name].min.js' // Archivo de salida
-	},
+		resolve: {
+			extensions: ['.js', '.jsx']
+		},
 
-	resolve: {
-	    extensions: ['.js', '.jsx', '.scss']
-	},
+		module: {
+			rules: [
+				{
+					test: /\.jsx$/,
+					exclude: /(node_modules|bower_components)/,
+					use: [{
+						loader: 'babel-loader',
+						options: {
+							presets: ['react', 'env', 'es2015']
+						}
+					}]
+				}
+			]
+		}
 
-	module: {
-	    rules: [
-		    {
-		        test: /\.jsx$/,
-		        exclude: /(node_modules|bower_components)/,
-		        use: [{
-		          	loader: 'babel-loader',
-		          	options: {
-		              	presets: ['react', 'env', 'es2015']
-		          	}
-		        }]
-			},
-			{
-				test: /\.scss$/,
-				use: [{
-					loader: 'style-loader', // inject CSS to page
-				  }, {
-					loader: 'css-loader', // translates CSS into CommonJS modules
-				  }, {
-					loader: 'postcss-loader', // Run post css actions
-					options: {
-					  plugins: function () { // post css plugins, can be exported to postcss.config.js
-						return [
-						  require('precss'),
-						  require('autoprefixer')
-						];
-					  }
-					}
-				  }, {
-					loader: 'sass-loader' // compiles Sass to CSS
-				  }]
-			}
-	    ]
-	},
+		devServer: {
+			contentBase: path.join(__dirname, './'),
+			historyApiFallback: true,	
+			port: 8080
+		},
 
-	devServer: {
-		contentBase: path.join(__dirname, './'),
-		historyApiFallback: true,	
-		port: 8080
-	},
-
-	devtool: 'inline-source-map',
+		devtool: 'inline-source-map',
+	}
+	
 };
-
-module.exports = config;
