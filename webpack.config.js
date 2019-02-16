e49bf8fe5dbb1c540fae59b6e3db1c0ea684de02
Jose Daniel <josedanq100@gmail.com>
2019-02-16 08:13:31 -0400

    install spectre as dependency and fix mysql bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,48 @@
-const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path')
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 // const webpack = require('webpack');
 
 module.exports = (env) => {
-	return {
-		// entry: './resources/assets/js/index.jsx',
-		entry: {
-			// config: './resources/assets/js/config.js',
-			index: './resources/assets/js/index.jsx',
-			backend: './resources/assets/js/backend.jsx'
-		},
+  return {
+    // entry: './resources/assets/js/index.jsx',
+    entry: {
+      // config: './resources/assets/js/config.js',
+      index: './resources/assets/js/index.jsx',
+      backend: './resources/assets/js/backend.jsx'
+    },
 
-		output: {
-			path: path.resolve(__dirname, 'public/js/'),
-			publicPath: '/public/js/',
-			filename: '[name].min.js' // Archivo de salida
-		},
+    output: {
+      path: path.resolve(__dirname, 'public/js/'),
+      publicPath: '/public/js/',
+      filename: '[name].min.js' // Archivo de salida
+    },
 
-		resolve: {
-			extensions: ['.js', '.jsx']
-		},
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
 
-		module: {
-			rules: [
-				{
-					test: /\.jsx$/,
-					exclude: /(node_modules|bower_components)/,
-					use: [{
-						loader: 'babel-loader',
-						options: {
-							presets: ['react', 'env', 'es2015', 'stage-0']
-						}
-					}]
-				}
-			]
-		},
+    module: {
+      rules: [
+        {
+          test: /\.jsx$/,
+          exclude: /(node_modules|bower_components)/,
+          use: [{
+            loader: 'babel-loader',
+            options: {
+              presets: ['react', 'env', 'es2015', 'stage-0']
+            }
+          }]
+        }
+      ]
+    },
 
-		devServer: {
-			contentBase: path.join(__dirname, './'),
-			historyApiFallback: true,	
-			port: 8080
-		},
+    devServer: {
+      contentBase: path.join(__dirname, './'),
+      historyApiFallback: true,
+      port: 8080
+    },
 
-		devtool: 'inline-source-map',
-	}
-	
-};
+    devtool: 'inline-source-map'
+  }
+}
